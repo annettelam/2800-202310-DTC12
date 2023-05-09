a9090dd15d7f5e9c67a22dee366cd0f73df3fea0
Mikko Sio <msio1@my.bcit.ca>
2023-05-09 13:34:35 -0700

    Implemented API response

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const port = 3000;
 
 app.use(express.urlencoded({ extended: false }));
 
+const hotelAPI = axios.create({
+    baseURL: 'https://booking-com.p.rapidapi.com/v1/hotels',
+    headers: {
+        'X-RapidAPI-Key': '756c633279msh0fadc5ba4579eefp126c2fjsn7844ff44c751',
+        'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
@@ -29,6 +37,50 @@ app.get('/hotels', (req, res) => {
             <input type="submit" value="Submit">
             </form>
             `);
+});
+
+app.post('/hotels', async (req, res) => {
+    const { city, checkin, checkout, guests } = req.body;
+
+    console.log(`City: ${city}`);
+    console.log(`Check In: ${checkin}`);
+    console.log(`Check Out: ${checkout}`);
+    console.log(`Guests: ${guests}`);
+
+    try {
+        const result = await hotelAPI.get('/locations', {
+            params: {
+                name: city,
+                locale: 'en-gb'
+            }
+        });
+
+        const { dest_id } = result.data[0];
+
+        console.log(dest_id);
+
+        const hotels = await hotelAPI.get('/search', {
+            params: {
+                checkin_date: checkin,
+                dest_type: 'city',
+                units: 'metric',
+                checkout_date: checkout,
+                adults_number: guests,
+                order_by: 'price',
+                dest_id: dest_id,
+                filter_by_currency: 'CAD',
+                locale: 'en-gb',
+                room_number: 1
+            }
+        });
+
+        res.json(hotels.data.result);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
 });
 
 app.listen(port, () => {
