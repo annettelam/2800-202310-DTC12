50fe9fd5a66e5ba950a3b3d51e0fd0fd037544af
annettelam <113328885+annettelam@users.noreply.github.com>
2023-05-09 01:54:42 -0700

    Add test logos with png

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,25 @@ import { Navbar, Nav, Container, Form, Button } from 'react-bootstrap';
 import { FaBars } from 'react-icons/fa';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.png';
+import alicelogo from './alicelogo.png';
+import navlogo from './navlogo.png';
 
 const Planetpass = () => {
   return (
-    <div>
-      <Navbar bg="" expand="lg">
+    <div style={{ backgroundColor: '#E6F7FF' }}>
+      <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">PlanetPass</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={navlogo} alt="logo" className="App-logo" style={{ width: '85px' }} />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0">
             <FaBars />
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="#flights">Find Flights</Nav.Link>
+              <Nav.Link href="#hotels">Find Hotels</Nav.Link>
             </Nav>
             <Form inline>
               <Button variant="outline-success" className="me-2">Sign Up</Button>
@@ -28,9 +31,8 @@ const Planetpass = () => {
         </Container>
       </Navbar>
       <div className="text-center my-5">
-        <h1>Welcome to PlanetPass!</h1>
-        <img src={logo} alt="logo" className="App-logo" />
-        <p>Please sign up or login to access the site.</p>
+        <img src={alicelogo} alt="logo" className="App-logo" style={{ width: '300px' }} />
+
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
