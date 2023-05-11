1f1219b8da33c2c763e84f300e1061845b603f27
Mikko Sio <msio1@my.bcit.ca>
2023-05-11 16:12:55 -0700

    Fixed bugs

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../fonts.css';
