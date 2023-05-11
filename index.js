dcd568765bc3548ac7318cd28ec2c9046d9e9c68
mimikwkk <113390005+mimikwkk@users.noreply.github.com>
2023-05-10 21:02:35 -0700

    Attempt to add thumbnail image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
+const path = require('path');
 const port = 3000;
 const googleMapsApiKey = 'AIzaSyAidVELYsMX22Ztp4SKJ0TzF15TX0Kqoss';
 const tripAdvisorApiKey = 'EB348A0AB91544309BA148014BE4F02B';
@@ -31,7 +32,10 @@ app.get('/search', (req, res) => {
         } else if (json.data.length === 0) {
             res.render('results', {location, attractions: []});
         } else {
-            const attractions = json.data.map(attraction => attraction.name);
+            const attractions = json.data.map(attraction => ({
+                name: attraction.name,
+                location_id: attraction.location_id
+              }));              
             res.render('results', {location, attractions});
         }
     })
@@ -40,6 +44,35 @@ app.get('/search', (req, res) => {
         res.status(500).send('An error occurred');
     });
 });
+
+app.get('/image', (req, res) => {
+    const locationId = req.query.location_id;
+    const tripAdvisorImgUrl = `https://api.content.tripadvisor.com/api/v1/location_photos/list?location_id=${encodeURIComponent(locationId)}&key=${tripAdvisorApiKey}&category=attractions&language=en`;
+    console.log(locationId)
+    fetch(tripAdvisorImgUrl)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json);
+            if (!json.data || !Array.isArray(json.data) || json.data.length === 0) {
+                res.sendFile(path.join(__dirname, 'public', 'no-photo.png'));
+            } else {
+                const photoUrl = json.data[0].images.medium.url;
+                return fetch(photoUrl, {
+                headers: {'X-TripAdvisor-API-Key': tripAdvisorApiKey}
+                });
+            }
+        })
+        .then(res => {
+            if (res) {
+                res.body.pipe(res);
+            }
+        })
+        .catch(err => {
+            console.error('error:' + err);
+            res.sendFile(path.join(__dirname, 'public', 'no-photo.png'));
+        });
+});
+  
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
