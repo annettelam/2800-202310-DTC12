e85eb0f2cecbcfabc0d056e0857f14b2ee1f19df
annettelam <113328885+annettelam@users.noreply.github.com>
2023-05-11 04:31:51 -0700

    Implement landing page with test image

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
-import '../../App.css';
+import { ChakraProvider, Box, Heading, Text, Button } from '@chakra-ui/react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import './home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../fonts.css';
 import { Footer } from '../footer/footer';
+import wireframe from '../../wireframe.png';
 
-export const Home = () => {
+export const Home = (props) => {
     return (
-        <div style={{ backgroundColor: '#E6F7FF', fontFamily: 'Questrial' }}>
-            <div className="text-center my-5">Hello
+        <ChakraProvider>
+            <br></br>
+            <br></br>
+            <div
+                style={{
+                    background: 'linear-gradient(to top, #0CBAA6, white)',
+                    fontFamily: 'Questrial',
+                    minHeight: '100vh',
+                }}
+            >
+                <Container fluid>
+                    <Row>
+                        <Col className="p-0" xs={6} md={6}>
+                            <div className="h-100 d-flex align-items-center justify-content-center">
+                                <Box maxW="32rem" textAlign="center">
+                                    <Heading mb={4}>LET'S BEGIN YOUR JOURNEY</Heading>
+                                    <Text fontSize="xl">Lorem ipsum dolor sit amet.</Text>
+                                    <Button size="lg" colorScheme="green" mt="24px">
+                                        Lorem ipsum.
+                                    </Button>
+                                </Box>
+                            </div>
+                        </Col>
+                        <Col className="p-0" xs={6} md={6}>
+                            <div className="h-100 d-flex align-items-center justify-content-center">
+                                <Image src={wireframe} alt="main2" fluid />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
             <Footer />
-        </div >
+        </ChakraProvider>
     );
 };
