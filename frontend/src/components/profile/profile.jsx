f03a535fef710b72b452e014bbea9146d75f41cb
annettelam <113328885+annettelam@users.noreply.github.com>
2023-05-12 02:32:47 -0700

    Implement profile button in navbar

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -1,75 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
-    ChakraProvider, Box, Heading, Text, Card, CardHeader,
-    CardBody, Stack, StackDivider, Image, CardFooter,
-    Button, Tabs, TabList, Tab, TabPanels, TabPanel
+    ChakraProvider, Heading, Container, Box, Card,
+    Divider, Avatar, Text
 } from '@chakra-ui/react';
-import '../home/home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../App.css';
 import '../../fonts.css';
 import { Footer } from '../footer/footer';
+import dashBackground from '../../dashbkg.jpg';
 
-export const Dashboard = (props) => {
+export const Profile = () => {
+    const navigate = useNavigate();
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    useEffect(() => {
+        if (localStorage.getItem('loggedIn') !== 'true') {
+            navigate('/login');
+        }
+    }, [navigate]);
+
     return (
         <ChakraProvider>
             <div
+                className="dashboard-container"
                 style={{
-                    background: 'linear-gradient(to bottom right, aliceblue, teal)',
+                    backgroundImage: `url(${dashBackground})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center top 50px', // Move background down 50 pixels
+                    backgroundAttachment: 'fixed',
+                    backgroundSize: 'cover',
                     fontFamily: 'Questrial',
                     minHeight: '100vh',
                 }}
             >
-                <Card align='center'>
-                    <CardHeader>
-                        <Heading size='md'>Welcome,</Heading>
-                    </CardHeader>
+                <Container>
+                    <Box p="4" boxShadow="lg" rounded="md" bg="aliceblue" mb="4">
+                        <Heading align="center">Welcome, {user.firstName} {user.lastName}!</Heading>
+                        <Text align="center" mt="2">
+                            Here are your account details.
+                        </Text>
+                    </Box>
 
-                    <CardBody>
-                        <Stack divider={<StackDivider />} spacing='4'>
-                            <Box>
-                                <Heading size='xs' textTransform='uppercase'>
-                                    Summary
-                                </Heading>
-                                <Text pt='2' fontSize='sm'>
-                                    View a summary of all your clients over the last month.
-                                </Text>
+                    <Card maxWidth="500px" mx="auto">
+                        <Box p="4" textAlign="center">
+                            <Box mx="auto">
+                                <Avatar size="md" />
                             </Box>
-                            <Box>
-                                <Heading size='xs' textTransform='uppercase'>
-                                    Overview
-                                </Heading>
-                                <Text pt='2' fontSize='sm'>
-                                    Check out the overview of your clients.
-                                </Text>
+                            <Divider my="4" />
+                            <Box display="flex" flexDirection="column" alignItems="center">
+                                <Box mb="2">
+                                    <strong>Username:</strong> {user.username}
+                                </Box>
+                                <Divider my="2" />
+                                <Box mb="2">
+                                    <strong>First Name:</strong> {user.firstName}
+                                </Box>
+                                <Divider my="2" />
+                                <Box mb="2">
+                                    <strong>Last Name:</strong> {user.lastName}
+                                </Box>
+                                <Divider my="2" />
+                                <Box mb="2">
+                                    <strong>Email:</strong> {user.email}
+                                </Box>
+                                <Divider my="2" />
+                                <Box>
+                                    <strong>City:</strong> {user.city}
+                                </Box>
                             </Box>
-                        </Stack>
-                    </CardBody>
-                </Card>
-                <Card align='center'>
-                    <CardHeader>
-                        <Heading size='md'> Customer dashboard</Heading>
-                    </CardHeader>
-                    <CardBody>
-                        <Text>View a summary of all your customers over the last month.</Text>
-                    </CardBody>
-                    <CardFooter>
-                        <Button colorScheme='blue'>View here</Button>
-                    </CardFooter>
-                </Card>
-                <Tabs variant='soft-rounded' colorScheme='green'>
-                    <TabList>
-                        <Tab>Tab 1</Tab>
-                        <Tab>Tab 2</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <p>one!</p>
-                        </TabPanel>
-                        <TabPanel>
-                            <p>two!</p>
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
+                        </Box>
+                    </Card>
+                </Container>
             </div>
             <Footer />
         </ChakraProvider>
