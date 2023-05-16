82214c58d3ecd172253df9f345253d9f35f16a9c
Mikko Sio <msio1@my.bcit.ca>
2023-05-16 16:15:26 -0700

    Updated front end

--- frontend/src/components/hotels/hotels.jsx
+++ frontend/src/components/hotels/hotels.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ChakraProvider, Container, Box, Heading, Text, FormControl, FormLabel, Input, Select, Button as ChakraButton } from '@chakra-ui/react';
+import { ChakraProvider, Container, Box, Heading, Text, FormControl, FormLabel, Input, Select, Button as ChakraButton, Flex, Image } from '@chakra-ui/react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../fonts.css';
 import dashBackground from '../../dashbkg.jpg';
 import cities from './cities';
+import { Button } from 'react-bootstrap';
+import { FaHeart } from 'react-icons/fa';
 
 export const Hotels = () => {
     const navigate = useNavigate();
@@ -16,6 +18,7 @@ export const Hotels = () => {
     const [numAdults, setNumAdults] = useState('');
     const [numRooms, setNumRooms] = useState('');
     const [hotels, setHotels] = useState({});
+    const [savedHotels, setSavedHotels] = useState([]);
 
     useEffect(() => {
         if (localStorage.getItem('loggedIn') !== 'true') {
@@ -23,6 +26,21 @@ export const Hotels = () => {
         }
     }, [navigate]);
 
+    const isHotelSaved = (hotelId) => {
+        return savedHotels.includes(hotelId);
+    };
+
+    const handleSaveHotel = async (hotelId) => {
+        console.log(hotelId);
+        console.log(isHotelSaved(hotelId));
+        if (!isHotelSaved(hotelId)) {
+            setSavedHotels([...savedHotels, hotelId]);
+        } else {
+            const newSavedHotels = savedHotels.filter((savedHotelId) => savedHotelId !== hotelId);
+            setSavedHotels(newSavedHotels);
+        }
+    };
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(city, checkInDate, checkOutDate, numAdults, numRooms);
@@ -146,28 +164,46 @@ export const Hotels = () => {
                         </form>
                     </Box>
                 </Container>
-                <Container className='hotel-results' maxWidth="6xl">
-                    {Object.keys(hotels).map((hotelId) => (
-                        <Box key={hotelId} p="4" boxShadow="lg" rounded="md" bg="aliceblue" mb="4">
-                            <Heading align="center">{hotels[hotelId].hotel_name}</Heading>
-                            <img src={hotels[hotelId].main_photo_url} alt={hotels[hotelId].hotel_name} style={{ width: '15%', height: 'auto' }} />
-                            <Text align="center" mt="2">
-                                <b>Price:</b> ${hotels[hotelId].min_total_price}
-                            </Text>
-                            <Text align="center" mt="2">
-                                <b>Address:</b> {hotels[hotelId].address}
-                            </Text>
-                            {hotels[hotelId].review_score !== null ? (
-                                <Text align="center" mt="2">
-                                    <b>Rating:</b> {hotels[hotelId].review_score} / 10
-                                </Text>
-                            ) : (
-                                <Text align="center" mt="2">
-                                    <b>Rating:</b> No reviews yet
-                                </Text>
-                            )}
-                        </Box>
-                    ))}
+                <Container className="hotel-results" maxWidth="6xl">
+                    {Object.keys(hotels).map((hotelId) => {
+                        const hotel = hotels[hotelId];
+                        const roundedPrice = hotel.min_total_price.toFixed(2);
+
+                        return (
+                            <Box key={hotelId} p="4" boxShadow="lg" rounded="md" bg="white" mb="4" position="relative">
+                                <Flex direction="column" align="center">
+                                    <Box position="absolute" top="15px" right="15px" onClick={() => handleSaveHotel(hotel.hotel_id)}>
+                                        <FaHeart 
+                                            size={30}
+                                            color={isHotelSaved(hotel.hotel_id) ? 'red' : 'black'}
+                                            fill={isHotelSaved(hotel.hotel_id) ? 'red' : 'none'}
+                                            stroke={isHotelSaved(hotel.hotel_id) ? 'none' : 'currentColor'}
+                                            strokeWidth="10"
+                                            style={{ cursor: 'pointer' }} />
+                                    </Box>
+                                    <Heading size={{base:'md', lg:'lg'}} w='75%' textAlign="center" mb="4">
+                                        {hotel.hotel_name}
+                                    </Heading>
+                                    <Image src={hotel.max_photo_url} alt={hotel.hotel_name} w={{base: '80%', sm: '65%', md: '55%', lg: '40%' }} h="auto" rounded="md" mb="4" />
+                                    <Text fontSize="lg" textAlign="center" mb="2">
+                                        <b>Price:</b> ${roundedPrice} CAD
+                                    </Text>
+                                    <Text fontSize="lg" textAlign="center" mb="2">
+                                        <b>Address:</b> {hotel.address}
+                                    </Text>
+                                    <Text fontSize="lg" textAlign="center" mb="4">
+                                        <b>Rating:</b> {hotel.review_score !== null ? `${hotel.review_score} / 10` : 'No reviews yet'}
+                                    </Text>
+                                    <Button
+                                        size="md"
+                                        onClick={() => window.location.href = hotel.url}
+                                    >
+                                        Book at Booking.com
+                                    </Button>
+                                </Flex>
+                            </Box>
+                        );
+                    })}
                 </Container>
             </div>
         </ChakraProvider>
