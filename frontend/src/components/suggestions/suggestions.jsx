0c2a641e96d941c74e56c2b8e375661a077735da
mimikwkk <113390005+mimikwkk@users.noreply.github.com>
2023-05-18 22:50:46 -0700

    add cityName to display as header

--- frontend/src/components/suggestions/suggestions.jsx
+++ frontend/src/components/suggestions/suggestions.jsx
@@ -15,25 +15,19 @@ import '../../fonts.css';
 import dashBackground from '../../dashbkg.jpg';
 import './suggestions.css';
 import axios from 'axios';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const Suggestions = () => {
   const navigate = useNavigate();
-  const [user, setUser] = useState({}); // [user, setUser
-
-  // Attractions
+  const [user, setUser] = useState({}); 
+  const [cityName, setCityName] = useState(''); 
   const [attractions, setAttractions] = useState([]);
-
 
   useEffect(() => {
     if (localStorage.getItem('loggedIn') !== 'true') {
       navigate('/login');
     }
     setUser(JSON.parse(localStorage.getItem('user')));
-
-    //   // if (cityName) {
-      //   //   fetchAttractions();
-      //   // }
   }, [navigate]);
 
   useEffect(() => {
@@ -45,9 +39,7 @@ export const Suggestions = () => {
         console.log('Suggestions Response: ', suggResponse);
         setAttractions(suggResponse.data.attractions);
         console.log('Sugg Response data', suggResponse.data.attractions)
-        // if (suggResponse.data) {
-        //   setAttractions(suggResponse.data.attractions);
-        // }
+        setCityName(suggResponse.data.cityName);
       } catch (err) {
         console.log(err);
       }
@@ -57,66 +49,72 @@ export const Suggestions = () => {
 
   return (
     <ChakraProvider>
-        <Box
-            minHeight="100vh"
-            backgroundImage={`url(${dashBackground})`}
-            backgroundSize="cover"
-            backgroundPosition="center"
-        >
-            <VStack minHeight="100vh" justifyContent="space-between">
-                <Box>
-                    <Flex
-                        flexDirection="column"
+      <Box
+          minHeight="100vh"
+          backgroundImage={`url(${dashBackground})`}
+          backgroundSize="cover"
+          backgroundPosition="center"
+      >
+        <VStack minHeight="100vh" justifyContent="space-between">
+          <Box>
+              <Flex
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  mt="100px"
+              >
+                <Heading
+                    mb="30px"
+                    fontSize={{ base: '3xl', md: '6xl' }}
+                    fontWeight="bold"
+                    color="white"
+                >
+                  Some places you might like
+                </Heading>
+              </Flex>
+              <Box mt={6}>
+                <Heading as="h3" size="lg" mb={4} textAlign="center">
+                    Attractions in {cityName}
+                </Heading>
+                {attractions.length === 0 ? (
+                    <Text>Please save a flight first and wait a couple seconds.</Text>
+                ) : (
+                  <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} spacing={10}>
+                    {attractions.map((attraction) => (
+                      <Box
+                      key={attraction.location_id}
+                      boxShadow="lg"
+                      rounded="md"
+                      overflow="hidden"
+                      >
+                        <Box
+                        display="flex"
                         justifyContent="center"
                         alignItems="center"
-                        mt="100px"
-                    >
-                    <Heading
-                        mb="30px"
-                        fontSize={{ base: '3xl', md: '6xl' }}
-                        fontWeight="bold"
-                        color="white"
-                    >
-                        Some places you might like
-                    </Heading>
-                    </Flex>
-                    <Box mt={6}>
-                    <Heading as="h3" size="lg" mb={4}>
-                        Attractions
-                    </Heading>
-                    {attractions.length === 0 ? (
-                        <Text>No attractions found. Please find a flight first.</Text>
-                    ) : (
-                        <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} spacing={10}>
-                        {attractions.map((attraction) => (
-                            <Box
-                            key={attraction.location_id}
-                            boxShadow="lg"
-                            rounded="md"
-                            overflow="hidden"
-                            >
-                            <Image
-                                src={attraction.photoUrl}
-                                h="250px"
-                                objectFit="cover"
-                                alt={attraction.name}
-                            />
-                            <VStack p="4" alignItems="start" spacing={2}>
-                                <Heading size="md">{attraction.name}</Heading>
-                            </VStack>
-                            </Box>
-                        ))}
-                        </SimpleGrid>
-                    )}
-                    </Box>
-                    </Box>
-                    <Box>
-                        <Heading as="h3" size="lg" mb={4}>
-                            Packing List
-                        </Heading>
-                    </Box>
-            </VStack>
+                        >
+                          <Image
+                            src={attraction.photoUrl}
+                            h="250px"
+                            objectFit="cover"
+                            alt={attraction.name}
+                          />
+                        </Box>
+                        <VStack p="4" alignItems="start" spacing={2}>
+                          <Heading size="md">{attraction.name}</Heading>
+                        </VStack>
+                      </Box>
+                    ))}
+                  </SimpleGrid>
+                )}
+              </Box>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg" mb={4}>
+                Packing List
+              </Heading>
+            </Box>
+          </VStack>
         </Box>
     </ChakraProvider>
-    );
+  );
 };
