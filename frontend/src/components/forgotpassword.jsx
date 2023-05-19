1b2b9971f28c35083b18f4552f766efa94c5d5ba
annettelam <113328885+annettelam@users.noreply.github.com>
2023-05-19 04:05:05 -0700

    Update background and call to action in reset password page

--- frontend/src/components/forgotpassword.jsx
+++ frontend/src/components/forgotpassword.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Form, Button, Alert } from 'react-bootstrap';
-import alicelogo from '../alicelogo.png';
+import navlogo from '../navlogo.png';
+import { ChakraProvider, Box, Image, Flex } from '@chakra-ui/react';
 
 export const ForgotPassword = () => {
     const [email, setEmail] = useState('');
@@ -18,37 +19,41 @@ export const ForgotPassword = () => {
     };
 
     return (
-        <div style={{ backgroundColor: '#E6F7FF', fontFamily: 'Questrial', minHeight: '100vh' }}>
-            <div className="text-center">
-                <img src={alicelogo} alt="logo" className="App-logo" style={{ width: '200px', marginTop: '30px', marginBottom: '30px' }} />                <Alert variant="primary">
-                    <Alert.Heading>Forgot Password</Alert.Heading>
-                    <hr />
-                    <p className="mb-0">
-                        Please enter your email address below.
-                        We will send you an email with instructions on how to reset your password.
-                    </p>
-                </Alert>
-                {emailSent && <div className="alert alert-success">Email sent successfully! Please check your email.</div>}
-                <div style={{ maxWidth: '400px', margin: '0 auto' }}>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control
-                                type="email"
-                                name="email"
-                                placeholder="Enter email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                style={{ backgroundColor: '#fff', border: '1px solid #ced4da', borderRadius: '4px' }}
-                            />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" style={{ width: '100%' }}>
-                            Reset Password
-                        </Button>
-                    </Form>
+        <ChakraProvider>
+            <Box bgGradient="linear(to bottom right, aliceblue, teal)" fontFamily="Questrial" minHeight="100vh">
+                <div className="text-center">
+                    <Flex justify="center">
+                        <Image src={navlogo} alt="Planetpass Logo" boxSize="250px" objectFit="contain" />
+                    </Flex>
+                    <Alert variant="primary">
+                        <Alert.Heading>Change / Forgot Password</Alert.Heading>
+                        <hr />
+                        <p className="mb-0">
+                            Please enter your email address below.
+                            We will send you an email with instructions on how to reset your password.
+                        </p>
+                    </Alert>
+                    {emailSent && <div className="alert alert-success">Email sent successfully! Please check your email.</div>}
+                    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    name="email"
+                                    placeholder="Enter email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    style={{ backgroundColor: '#fff', border: '1px solid #ced4da', borderRadius: '4px' }}
+                                />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" style={{ width: '100%' }}>
+                                Reset Password
+                            </Button>
+                        </Form>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </Box>
+        </ChakraProvider>
     );
-
 };
