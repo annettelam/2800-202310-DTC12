6d473bc52c8caab110a75fc73aeb469d6c396490
lisajung <lisajung998@gmail.com>
2023-05-23 02:15:58 -0700

    Place saved flights card into carousel

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,17 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {Box, Container, Heading, Text, Grid, ChakraProvider,} from '@chakra-ui/react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  ChakraProvider,
+  PopoverFooter,
+  PopoverBody,
+  PopoverCloseButton,
+  Button,
+  Center
+} from '@chakra-ui/react';
+import '../home/home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../fonts.css';
 import { FaHeart } from 'react-icons/fa';
 import axios from 'axios';
-import bkg3 from '../../bkg.jpg';
+import bkg from '../../bkg.jpg';
+import './dashboard.css';
+import {
+  Heading,
+  Text,
+  VStack,
+  Image,
+  Box,
+} from '@chakra-ui/react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import landingPlane from '../flights/landing-airplane.png';
 import takeoffPlane from '../flights/takeoff-airplane.png';
 import { formatTime, formatDuration, formatStopDisplay } from '../flights/flights.jsx';
+
 
 export const Dashboard = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState({});
   const [savedFlights, setSavedFlights] = useState([]);
+  const [cityName, setCityName] = useState('');
+  const [attractions, setAttractions] = useState([]);
 
   useEffect(() => {
     if (localStorage.getItem('loggedIn') !== 'true') {
@@ -58,58 +85,81 @@ export const Dashboard = () => {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    const fetchAttractions = async () => {
+      try {
+        const suggResponse = await axios.post('http://localhost:4000/suggestions', {
+          user
+        });
+        setAttractions(suggResponse.data.attractions);
+        setCityName(suggResponse.data.cityName);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAttractions();
+  }, [user]);
+
+
   return (
     <ChakraProvider>
       <div
         className="dashboard-container"
         style={{
-          backgroundImage: `url(${bkg3})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center top 50px', // Move background down 50 pixels
-          backgroundAttachment: 'fixed',
-          backgroundSize: 'cover',
-          fontFamily: 'Questrial',
-          minHeight: '100vh',
+          backgroundImage: `url(${bkg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center top 50px", // Move background down 50 pixels
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+          fontFamily: "Questrial",
+          minHeight: "95vh",
+          overflowX: "auto", // Enable horizontal scrolling
+          padding: "2rem 0", // Add top and bottom padding
         }}
       >
-        <Container maxWidth="6xl">
-          {/* Header for Saved Flights */}
-          <div className="header-section">
-            <Heading as="h1" size="xl" mb="4" textAlign="center">
-              Saved Flights
-            </Heading>
-          </div>
+        <Container fluid className="px-3">
+          <Row className="flex-nowrap">
+            {" "}
+            {/* Add flex-nowrap class to prevent wrapping */}
+            <Col>
+              <h2 className="mb-4">Flights</h2> {/* Category heading */}
+              <Carousel 
+                showThumbs={false}
+                showStatus={false}
+                infiniteLoop={true}
+                interval={3000} // Adjust the interval as needed
+              >
+                {/* Wrap the scrollable content */}
+                {savedFlights.map((flight) => (
+                  <Box key={flight.id} p="4" boxShadow="lg" rounded="md" bg="white" mb="4" position="relative">
+                    <Box align="right" onClick={() => handleSaveFlight(flight.id)}>
+                      <FaHeart
+                        size={30}
+                        color={isFlightSaved(flight.id) ? 'red' : 'black'}
+                        fill={isFlightSaved(flight.id) ? 'red' : 'none'}
+                        stroke={isFlightSaved(flight.id) ? 'none' : 'currentColor'}
+                        strokeWidth="10"
+                        style={{ cursor: 'pointer' }}
+                      />
+                    </Box>
+                    <Heading>${flight.price.amount.toFixed(2)}</Heading>
 
-          <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-            {/* Display Flight Results */}
-            {savedFlights.map((flight) => (
-              <Box key={flight.id} p="4" boxShadow="lg" rounded="md" bg="white" mb="4" position="relative">
-                <Box align="right" onClick={() => handleSaveFlight(flight.id)}>
-                  <FaHeart
-                    size={30}
-                    color={isFlightSaved(flight.id) ? 'red' : 'black'}
-                    fill={isFlightSaved(flight.id) ? 'red' : 'none'}
-                    stroke={isFlightSaved(flight.id) ? 'none' : 'currentColor'}
-                    strokeWidth="10"
-                    style={{ cursor: 'pointer' }}
-                  />
-                </Box>
-                <Heading>${flight.price.amount.toFixed(2)}</Heading>
+                    {/* Eco flight information */}
+                    {flight.is_eco_contender && (
+                      <Box>
+                        <Text align="left" fontWeight="bold" fontSize="lg" mb="0">
+                          <span style={{ color: 'green' }}>Eco Flight</span>
+                        </Text>
+                        <Text align="left">
+                          <span style={{ color: 'green' }}>
+                            Produces <b>{Math.round(Math.abs(flight.eco_contender_delta))}%</b> less carbon emissions compared to similar flights.
+                          </span>
+                        </Text>
+                      </Box>
+                    )}
 
-                {/* Eco flight information */}
-                {flight.is_eco_contender && (
-                  <Box>
-                    <Text align="left" fontWeight="bold" fontSize="lg" mb="0">
-                      <span style={{ color: 'green' }}>Eco Flight</span>
-                    </Text>
-                    <Text align="left">
-                      <span style={{ color: 'green' }}>
-                        Produces <b>{Math.round(Math.abs(flight.eco_contender_delta))}%</b> less carbon emissions compared to similar flights.
-                      </span>
-                    </Text>
-                  </Box>
-                )}
-
+                    
                 {/* Iterate over legs and display departure times */}
                 {flight.legs.map((leg, index) => (
                   <Box key={index}>
@@ -166,27 +216,113 @@ export const Dashboard = () => {
                     </svg>
                   </Box>
                 ))}
-              </Box>
-            ))}
-          </Grid>
 
-        </Container>
+                  </Box>
+                ))}
 
-        <Container maxWidth="6xl">
-          {/* Header for Saved Hotels */}
-          <div className="header-section">
-            <Heading as="h2" size="xl" mb="2" textAlign="center">
-              Saved Hotels
-            </Heading>
-          </div>
+              </Carousel>
+            </Col>
+          </Row>
+
+          {/* Repeat the above structure for other categories */}
+          <Row className="flex-nowrap">
+            {" "}
+            {/* Add flex-nowrap class to prevent wrapping */}
+            <Col>
+              <h2 className="mb-4">Hotels</h2> {/* Category heading */}
+              <div className="horizontal-scroll">
+                {" "}
+                {/* Wrap the scrollable content */}
+                {/* ... Hotel cards */}
+              </div>
+            </Col>
+          </Row>
+          {/* Add other categories here */}
+          <Row className="flex-nowrap">
+            <Col style={{ zIndex: 1 }}>
+              <h2>Attractions in {cityName}</h2>
+              <Carousel
+                showThumbs={false}
+                showStatus={false}
+                infiniteLoop={true}
+                interval={3000} // Adjust the interval as needed
+              >
+                {/* Wrap the scrollable content */}
+                {attractions.length === 0 ? (
+                  <Text>
+                    Please save a flight first and wait a couple seconds.
+                  </Text>
+                ) : (
+                  attractions.map((attraction) => (
+                    <Col
+                      key={attraction.location_id}
+                      style={{ minWidth: "200px", maxWidth: "300px" }}
+                    >
+                      <Card
+                        className="m-2"
+                        boxShadow="lg"
+                        rounded="md"
+                        overflow="hidden"
+                      >
+                        <Box
+                          display="flex"
+                          justifyContent="center"
+                          alignItems="center"
+                        >
+                          <Image
+                            src={attraction.photoUrl}
+                            w="200px"
+                            objectFit="cover"
+                            alt={attraction.name}
+                          />
+                        </Box>
+                        <VStack p="1" alignItems="start" spacing={2}>
+                          <Heading size="md" alignItems="center">{attraction.name}</Heading>
+                        </VStack>
+                        <Popover
+
+                          placement="top-start"
+                          closeOnBlur={false}
+                        >
+                          <PopoverTrigger>
+                            <Button className="questrial-font">
+                              Learn More
+                            </Button>
+                          </PopoverTrigger>
+                          <PopoverContent
+                            color="white"
+                            bg="blue.700"
+                            borderColor="blue.800"
+                            w="250px"
+                          >
+                            <PopoverCloseButton />
+                            <PopoverBody
+                              className="questrial-font"
+                              style={{ overflowWrap: "break-word" }}
+                            >
+                              {attraction.description}
+                            </PopoverBody>
+                            <PopoverFooter
+                              border="0"
+                              display="flex"
+                              alignItems="center"
+                              justifyContent="space-between"
+                              pb={4}
+                            ></PopoverFooter>
+                          </PopoverContent>
+                        </Popover>
+                      </Card>
+                    </Col>
+                  ))
+                )}
+              </Carousel>
+            </Col>
+          </Row>
         </Container>
-        );
       </div>
     </ChakraProvider>
   );
 };
-
-
 
 
 
