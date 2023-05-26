a8aacd4520c141ad3b5bd8344b3ca4b1afc508ab
Mikko Sio <msio1@my.bcit.ca>
2023-05-26 13:05:42 -0700

    Fixed footer

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -8,11 +8,7 @@ export const Footer = () => {
         <footer className="bg-light text-center text-lg-start">
             <Container>
                 <div className="text-center p-3">
-                    © 2023 PlanetPass by 
-                    <span><em><span style={{ fontFamily: 'Charmonman' }}>
-                        aether
-                    </span></em></span>
-                    . All Rights Reserved.
+                    © 2023 PlanetPass by <span><em><span style={{ fontFamily: 'Charmonman' }}>aether</span></em></span>. All Rights Reserved.
                 </div>
             </Container>
         </footer>
